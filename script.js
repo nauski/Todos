f08db8c54bf1db989295fd29212d98bdfab68eb2
nauski <nauski@nauski.com>
2016-05-11 20:51:41 +0300

    filled crud placeholders, a bit

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ var todos = {
     todosList: [],
 
     createTodos: function(name) {
-        this.todosList.push(name);
+        var todo = {name: name, completed: false};
+        this.todosList.push(todo);
+        this.readTodos();
     },
     readTodos: function() {
-        for(var x = 0, y = this.todosList.length; x < y; x++){
-          console.log("[ ]", this.todosList[x]);
+        for (var x = 0, y = this.todosList.length; x < y; x++) {
+            console.log("#" + (x + 1), this.todosList[x].name, "\t \t \t [ ]");
         }
     },
 
-    updateTodos: function() {
-
+    updateTodos: function(num, info) {
+      this.todosList[num].name = info;
+      this.readTodos();
     },
-    deleteTodos: function() {
-
+    deleteTodos: function(num) {
+      this.todosList.splice(num, 1);
+      this.readTodos();
     }
 
 
