1e0f164e82a9eada24213a94f9731b0ff02d15c4
nauski <nauski@nauski.com>
2016-05-12 15:55:56 +0300

    mapped toggle button and added logic to toggle function

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
 var todos = {
-    todosList: [],
-
-    toggle: function() {
-
-    },
-    createTodos: function(name) {
-        var todoName = prompt("Description?");
-        var todo = {
-            name: todoName,
-            completed: false
-        };
-        this.todosList.push(todo);
-        this.readTodos(this.todosList);
-    },
-    readTodos: function(todosList) {
-        var line = "";
-        var toggleButton;
-        for (var x = 0, y = todosList.length; x < y; x++) {
-            if (todosList[x].completed) {
-                toggleButton = "Y";
-            } else {
-                toggleButton = "N";
+        todosList: [],
+        toggleAllTodos: function() {
+            var totalTodos = this.todosList.length;
+            var completedTodos = 0;
+            for (var i = 0; i < totalTodos; i++) {
+                if (this.todosList[i].completed === true) {
+                    completedTodos++
+                }
             }
-            line += "<button id=" + "btn" + todosList[x] + ">" + toggleButton + "</button> " + todosList[x].name + "<br>";
+            if (completedTodos === totalTodos) {
+                for (var i = 0; i < this.todosList.length; i++) {
+                    this.todosList[i].completed = false;
+                }
+            } else {
+                for (var i = 0; i < this.todosList.length; i++) {
+                    this.todosList[i].completed = true;
+                }
+
+              }
+              this.readTodos(this.todosList);
+            },
+            toggleCompleted: function(todonum) {
+                    var todo = this.todosList[todonum];
+                    todo.completed = !todo.completed;
+                    this.readTodos(this.todosList);
+                },
+                createTodos: function(name) {
+                    var todoName = prompt("Description?");
+                    var todo = {
+                        name: todoName,
+                        completed: false
+                    };
+                    this.todosList.push(todo);
+                    this.readTodos(this.todosList);
+                },
+                readTodos: function(todosList) {
+                    var line = "";
+                    var toggleButton, completedInfo;
+                    for (var x = 0, y = todosList.length; x < y; x++) {
+                        if (todosList[x].completed) {
+                            toggleButton = "Y";
+                            completedInfo = "[X]";
+                        } else {
+                            toggleButton = "N";
+                            completedInfo = "[ ]";
+                        }
+                        line += "<button id=" + "btn" + todosList[x] + ">" + toggleButton + "</button> " + todosList[x].name + "           " + completedInfo + "<br>";
+                    }
+                    document.getElementById("todoList").innerHTML = line;
+                },
+                updateTodos: function() {
+                    var num = prompt("Which one?");
+                    var info = prompt("Description?");
+                    this.todosList[num].name = info;
+                    this.readTodos(this.todosList);
+                },
+                deleteTodos: function() {
+                    var num = prompt("Which one?");
+                    this.todosList.splice(num, 1);
+                    this.readTodos(this.todosList);
+                }
+
         }
-        document.getElementById("todoList").innerHTML = line;
-    },
-    updateTodos: function() {
-        var num = prompt("Which one?");
-        var info = prompt("Description?");
-        this.todosList[num].name = info;
-        this.readTodos(this.todosList);
-    },
-    deleteTodos: function() {
-        var num = prompt("Which one?");
-        this.todosList.splice(num, 1);
-        this.readTodos(this.todosList);
-    }
-
-
-}
-var toggle = document.getElementById("toggle").addEventListener("click", function() {
-    todos.toggle();
-});
-var createTodoButton = document.getElementById("createTodo").addEventListener("click", function() {
-    todos.createTodos();
-});
-var updateTodoButton = document.getElementById("updateTodo").addEventListener("click", function() {
-    todos.updateTodos();
-});
-var deleteTodoButton = document.getElementById("deleteTodo").addEventListener("click", function() {
-    todos.deleteTodos();
-});
+        var toggle = document.getElementById("toggle").addEventListener("click", function() {
+            todos.toggleAllTodos();
+        });
+        var createTodoButton = document.getElementById("createTodo").addEventListener("click", function() {
+            todos.createTodos();
+        });
+        var updateTodoButton = document.getElementById("updateTodo").addEventListener("click", function() {
+            todos.updateTodos();
+        });
+        var deleteTodoButton = document.getElementById("deleteTodo").addEventListener("click", function() {
+            todos.deleteTodos();
+        });
